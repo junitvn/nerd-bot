471f8cdab8cbea9f16de77e43ca058564eaf1b05
Lam Nguyen <lam.nguyen7@mservice.com.vn>
2023-09-12 00:33:19 +0700

    build: Init project

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,51 +1,84 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-import useScroll from "@/lib/hooks/use-scroll";
-import { useSignInModal } from "./sign-in-modal";
-import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
+import { useState } from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { NavbarMenuModel } from "configs/NavBarConfig";
 
-export default function NavBar({ session }: { session: Session | null }) {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
-  const scrolled = useScroll(50);
+export default function NavBar({
+  session,
+  navbarMenu,
+}: {
+  session: Session | null;
+  navbarMenu: NavbarMenuModel[];
+}) {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <>
-      <SignInModal />
-      <div
-        className={`fixed top-0 w-full flex justify-center ${
-          scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-            : "bg-white/0"
-        } z-30 transition-all`}
-      >
-        <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
-          <Link href="/" className="flex items-center font-display text-2xl">
-            <Image
-              src="/logo.png"
-              alt="Precedent logo"
-              width="30"
-              height="30"
-              className="mr-2 rounded-sm"
-            ></Image>
-            <p>Precedent</p>
-          </Link>
-          <div>
-            {session ? (
-              <UserDropdown session={session} />
-            ) : (
-              <button
-                className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-                onClick={() => setShowSignInModal(true)}
-              >
-                Sign In
-              </button>
-            )}
+    <nav className="fixed top-0 z-50 w-full bg-[#1E252B]">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-300 hover:text-black focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              {isOpen ? (
+                <XIcon className="block h-6 w-6" aria-hidden="true" />
+              ) : (
+                <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+              )}
+            </button>
+          </div>
+          <div className="flex flex-1 items-center justify-start sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+              <a href="/">
+                <Image
+                  src={`/nerd-logo-white.png`}
+                  alt="N"
+                  width="100"
+                  height="70"
+                  priority={true}
+                />
+              </a>
+            </div>
+          </div>
+          <div className="hidden sm:ml-6 sm:block">
+            <div className="flex justify-end space-x-4">
+              {navbarMenu.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="rounded-md px-3 py-2 text-base font-semibold text-white hover:bg-gray-700 hover:text-white"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </>
+
+      <div
+        className={`${isOpen ? "block" : "hidden"} sm:hidden`}
+        id="mobile-menu"
+      >
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {navbarMenu.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="block rounded-md px-3 py-2 text-base font-semibold text-white hover:bg-gray-700 hover:text-white"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
   );
 }
---
+++ configs/NavBarConfig.ts
@@ -0,0 +1,22 @@
+export interface NavbarMenuModel {
+  name: string;
+  href: string;
+}
+export const NavbarMenu = [
+  {
+    name: "NerdBot",
+    href: "/",
+  },
+  {
+    name: "Pricing",
+    href: "/#pricing",
+  },
+  {
+    name: "Tokenomics",
+    href: "/#tokenomics",
+  },
+  {
+    name: "Roadmap",
+    href: "/#roadmap",
+  },
+];
